4bcf7d6212fbe85afc9e2f9eaba2421876c7d546
Lyzin <2638583016@qq.com>
2021-06-09 14:37:38 +0800

    添加docker内容

--- pushnewcode.sh
+++ pushnewcode.sh
@@ -16,21 +16,25 @@ function runstatus(){
 
 function pushgitee(){
     # 添加文件，默认添加所有文件
-    echo "添加文件:"
+    echo "\033[1;36m添加上传文件\033[0m"
     git add . 
+    echo -n "\033[1;36m添加上传文件:\033[0m"
     runstatus
     echo
 
     # 填写提交commit
-    echo "填写提交commit:"
+    echo "\033[1;36m添加提交commit\033[0m"
     git commit -m "$*" 
+    echo "\033[1;36m添加提交commit:\033[0m"
     runstatus
     echo
 
     # 推送到远程
-    echo "推送到远程:"
-    git push origin master 
+    echo "\033[1;36m推送到远程\033[0m"
+    git push origin master
+    echo "\033[1;36m推送到远程:\033[0m"
     runstatus
+    echo
 }
 
 function main(){
